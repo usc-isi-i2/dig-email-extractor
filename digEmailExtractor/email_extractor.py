9ec2d850b2758458c203c08de82b4344a239e02e
ZwEin <zwein27@gmail.com>
2016-10-02 15:35:25 -0700

    delete set_renamed_input_fields

--- digEmailExtractor/email_extractor.py
+++ digEmailExtractor/email_extractor.py
@@ -2,7 +2,7 @@
 # @Author: ZwEin
 # @Date:   2016-06-21 12:36:47
 # @Last Modified by:   ZwEin
-# @Last Modified time: 2016-10-02 15:25:48
+# @Last Modified time: 2016-10-02 15:32:04
 
 import copy 
 import types
@@ -28,10 +28,4 @@ class EmailExtractor(Extractor):
 
     def get_renamed_input_fields(self):
         return self.renamed_input_fields
-
-    def set_renamed_input_fields(self, renamed_input_fields):
-        if not (isinstance(renamed_input_fields, basestring) or isinstance(renamed_input_fields, types.ListType)):
-            raise ValueError("renamed_input_fields must be a string or a list")
-        self.renamed_input_fields = renamed_input_fields
-        return self 
 
